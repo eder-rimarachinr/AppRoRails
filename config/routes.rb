0da5988523089c62fb4d2d46a558bfab9cc7153e
joaquin <ederrr2802@gmail.com>
2023-02-27 01:05:22 -0500

    add methos http on controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
+  post '/products', to: 'products#create'
+  get '/products/new', to: 'products#new', as: :new_product
   get '/products', to: 'products#index'
+  get '/products/:id', to: 'products#show', as: :product
 
-
-  
 end
